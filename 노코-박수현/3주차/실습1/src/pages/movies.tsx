bd735e8e1137ceaf6882324fa97d651091794c5d
strfunctionk <comzun01@gmail.com>
2025-04-02 17:32:19 +0900

    feat:실습1 tmdb 추가

--- 노코-박수현/3주차/실습1/src/pages/movies.tsx
+++ 노코-박수현/3주차/실습1/src/pages/movies.tsx
@@ -1,12 +1,40 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Movie, MovieResponse } from '../types/movie';
 
-// movies.tsx
+import axios from 'axios';
+
 const MoviesPage = () => {
-  const params = useParams();
-  
-  console.log(params);
-  
-  return <h1>{params.movieId}번의 Movies Page 야호~!</h1>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  console.log(movies);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const { data } = await axios.get<MovieResponse>(
+        `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2FkOTZmNzQ5NWRmNTI5MmY1YzBkNjI0M2E2NTM2ZCIsIm5iZiI6MTc0MzU2MzAyOC42NTY5OTk4LCJzdWIiOiI2N2VjYTkxNDhmZWQzZjAyMzZhYWRlOGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Da2EKAMm53rElHbl66pHQtmxo69pWtMpQ_BwgREAxyg`,
+          },
+        }
+      );
+
+        setMovies(data.results); // 영화 데이터
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <ul>
+      {/* 옵셔널 체인 활용 */}
+      {movies?.map((movie) => (
+        <li key={movie.id}>
+          <h1>{movie.title}</h1>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MoviesPage;
