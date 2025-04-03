3cbeae87152852931c4e86c9eed67698f6997fb4
Kimtaeyeon04 <te4939@naver.com>
2025-04-03 11:27:22 +0900

    로딩 스피너 적용 완료

--- 연두-김태연/3nd-mission2/src/pages/MovieUpComig.tsx
+++ 연두-김태연/3nd-mission2/src/pages/MovieUpComig.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
 import Movie from "../types/movie";
+import Loading from "../components/Loading";
 
 export default function MovieUpComig(): JSX.Element {
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchMovies = async (): Promise<void> => {
+            setLoading(true);
+            await new Promise(resolve => setTimeout(resolve, 100)); 
+
             try {
                 const { data } = await axios.get(
                     "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
@@ -19,6 +24,8 @@ export default function MovieUpComig(): JSX.Element {
                 );
                 if (data && data.results) {
                     setMovies(data.results);
+                    setLoading(false);
+
                 }
             } catch (error) {
                 console.error("Error fetching movies:", error);
@@ -29,6 +36,7 @@ export default function MovieUpComig(): JSX.Element {
 
     return (
         <div className="max-w-6xl mx-auto p-5">
+            {loading ? <Loading /> : null} 
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
                 {movies.map((movie) => (
                     <MovieCard key={movie.id} movie={movie} />
