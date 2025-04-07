05accd410d6074465602cb71bc78cfaa7926ce25
Kimtaeyeon04 <te4939@naver.com>
2025-04-08 02:19:35 +0900

    레이아웃만 잡음 미션2

---
+++ 연두-김태연/4nd-mission2/src/components/header.tsx
@@ -0,0 +1,13 @@
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import axios from "axios";
+// import Loading from "../components/Loading";
+
+export default function headers(): JSX.Element {
+  
+  return (
+   <>
+   
+   </>
+  );
+};
