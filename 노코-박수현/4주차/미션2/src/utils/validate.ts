9c11830f48605428c5e3aa3c152423cb8cb0d43c
strfunctionk <comzun01@gmail.com>
2025-04-12 21:12:43 +0900

    feat:미션2 범위 수정

--- 노코-박수현/4주차/미션2/src/utils/validate.ts
+++ 노코-박수현/4주차/미션2/src/utils/validate.ts
@@ -11,7 +11,7 @@ function validateUser(values: userSinginInformation) {
     if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(values.email)) {
         errors.email = "이메일 형식이 아닙니다.";
     }
-    if(!(values.password.length > 8 && values.password.length < 20)) {
+    if (!(values.password.length >= 8 && values.password.length <= 20)) {
         errors.password = "비밀번호는 8자~20자 사이를 입력하셔야 합니다.";
     }
     return errors;
