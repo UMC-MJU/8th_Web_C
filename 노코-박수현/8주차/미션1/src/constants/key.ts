f6efda3aa9e986377965dd20e436dd0e40104705
strfunctionk <comzun01@gmail.com>
2025-05-25 22:42:33 +0900

    미션1 생성

---
+++ 노코-박수현/8주차/미션1/src/constants/key.ts
@@ -0,0 +1,12 @@
+export const LOCAL_STORAGE_KEY = {
+    accessToken: "accessToken",
+    refreshToken: "refreshToken",
+}
+
+export const QUERY_KEY = {
+    lps: "lps",
+    lpDetail: "lpDetail",
+    comments: "lpComments",
+    myInfo: "myInfo",
+  };
+  
