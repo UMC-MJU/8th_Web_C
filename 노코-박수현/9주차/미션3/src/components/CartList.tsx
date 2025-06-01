19315262280a327c2865aec796f78c1b0d68fb26
strfunctionk <comzun01@gmail.com>
2025-06-01 19:34:27 +0900

    feat: empty

--- 노코-박수현/9주차/미션3/src/components/CartList.tsx
+++ 노코-박수현/9주차/미션3/src/components/CartList.tsx
@@ -6,6 +6,9 @@ const CartList = () => {
 
     return (
         <div className="flex flex-col items-center justify-center">
+            {cartItems.length === 0 && <div className="my-10">
+                <p className="text-2xl font-semibold">장바구니가 비어있습니다.</p>
+            </div>}
             <ul>
                 {cartItems.map((item) => (
                     <CartItem key={item.id} lp={item} />
